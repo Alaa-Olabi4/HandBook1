396d3a4bc4c26a866256a8fa05ee87aaf3739cb7
Mhd Alaa Al-Olabi <90989140+Alaa-Olabi4@users.noreply.github.com>
2026-02-17 23:54:51 +0300

    Refactor import statements and articles array

--- articles/services/index.js
+++ articles/services/index.js
@@ -3,21 +3,21 @@
 // import superclip from './superclip.js';
 
 // import AnnualValidity from './AnnualValidity.js'
-import DonTdisturb from './Don\'tdisturb.js';
-import ConferenceJS from './Conference.JS';
-import RequestedSuperClipJS from './RequestedSuperClip.JS';
-import SMSJS from './SMS.JS';
-import VendingMachineJS from './VendingMachine.JS';
-import WaitingJS from './Waiting.JS';
+// import DonTdisturb from './Don\'tdisturb.js';
+// import ConferenceJS from './Conference.JS';
+// import RequestedSuperClipJS from './RequestedSuperClip.JS';
+// import SMSJS from './SMS.JS';
+// import VendingMachineJS from './VendingMachine.JS';
+// import WaitingJS from './Waiting.JS';
 import Barinoutlost from './Barinoutlost.js';
 import Callme from './Callme.js';
 import SuperClipPre from './SuperClipPre.js';
-import CallForwardPre from './CallForwardPre.js';
+// import CallForwardPre from './CallForwardPre.js';
 import CallScreeningPre from './CallScreeningPre.js';
 import SuperClipPost from './SuperClipPost.js';
-import CallForwardPost from './CallForwardPost.js';
+// import CallForwardPost from './CallForwardPost.js';
 import CallScreeningPost from './CallScreeningPost.js';
-import AnnualValidity from './AnnualValidity.js';
+// import AnnualValidity from './AnnualValidity.js';
 
 
 
@@ -47,3 +47,4 @@ const articles = [
 
 
 export default articles;
+
