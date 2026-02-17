5017dce486c6401869e78d870252dc57dee03ccb
Mhd Alaa Al-Olabi <90989140+Alaa-Olabi4@users.noreply.github.com>
2026-02-18 00:16:44 +0300

    Update index.js

--- articles/services/index.js
+++ articles/services/index.js
@@ -1,50 +1,51 @@
 // articles/services/index.js
-// import subscription from './subscription.js';
-// import superclip from './superclip.js';
+import subscription from './subscription.js';
+import superclip from './superclip.js';
 
-// import AnnualValidity from './AnnualValidity.js'
-// import DonTdisturb from './Don\'tdisturb.js';
-// import ConferenceJS from './Conference.JS';
-// import RequestedSuperClipJS from './RequestedSuperClip.JS';
-// import SMSJS from './SMS.JS';
-// import VendingMachineJS from './VendingMachine.JS';
-// import WaitingJS from './Waiting.JS';
+import AnnualValidity from './AnnualValidity.js'
+import DonTdisturb from './Don\'tdisturb.js';
+import ConferenceJS from './Conference.JS';
+import RequestedSuperClipJS from './RequestedSuperClip.JS';
+import SMSJS from './SMS.JS';
+import VendingMachineJS from './VendingMachine.JS';
+import WaitingJS from './Waiting.JS';
 import Barinoutlost from './Barinoutlost.js';
 import Callme from './Callme.js';
 import SuperClipPre from './SuperClipPre.js';
-// import CallForwardPre from './CallForwardPre.js';
+import CallForwardPre from './CallForwardPre.js';
 import CallScreeningPre from './CallScreeningPre.js';
 import SuperClipPost from './SuperClipPost.js';
-// import CallForwardPost from './CallForwardPost.js';
+import CallForwardPost from './CallForwardPost.js';
 import CallScreeningPost from './CallScreeningPost.js';
-// import AnnualValidity from './AnnualValidity.js';
+import AnnualValidity from './AnnualValidity.js';
 
 
 
 // يمكنك إضافة الترتيب الذي تريده هنا
 const articles = [
-    // subscription,
-    // superclip,
+    subscription,
+    superclip,
     
-    // DonTdisturb ,
-    // ConferenceJS ,
-    // RequestedSuperClipJS , 
-    // SMSJS ,
-    // VendingMachineJS , 
-    // WaitingJS ,
+    DonTdisturb ,
+    ConferenceJS ,
+    RequestedSuperClipJS , 
+    SMSJS ,
+    VendingMachineJS , 
+    WaitingJS ,
     Barinoutlost , 
-    // AnnualValidity,    
+    AnnualValidity,    
     Callme ,
     SuperClipPre , 
-    // CallForwardPre ,
+    CallForwardPre ,
     CallScreeningPre , 
     SuperClipPost ,
-    // CallForwardPost ,
+    CallForwardPost ,
     CallScreeningPost , 
-    // AnnualValidity
+    AnnualValidity
 
 ];
 
 
 export default articles;
+
 
